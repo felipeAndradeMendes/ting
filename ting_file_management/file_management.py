6595265e0d3de594eb280792dd96923fbfe963e9
Felipe Mendes <felipemadruga@gmail.com>
2023-08-28 15:38:03 -0300

    Conclui requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,23 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    invalid_format_error = "Formato inválido"
+    file_not_found_error = f"Arquivo {path_file} não encontrado"
+
+    if path_file.split(".")[1] != "txt":
+        print(invalid_format_error, file=sys.stderr)
+
+    try:
+        with open(path_file, mode="r") as file:
+            data = file.readlines()
+    except FileNotFoundError:
+        print(file_not_found_error, file=sys.stderr)
+
+    else:
+        list_of_news = [line.replace('\n', '') for line in data]
+        return list_of_news
+
+
+print(txt_importer("news_file.txt"))
+# print(txt_importer("news_file.csv"))
