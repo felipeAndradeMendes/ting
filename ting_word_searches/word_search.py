f1f112dbf351749cec29c60a21fd3d312b965a03
Felipe Mendes <felipemadruga@gmail.com>
2023-08-29 11:46:21 -0300

    Refatora requisitos 07 e 08
    Flake acusa problema de complexidade em def search_in_fila

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,7 +2,7 @@
 # from ting_file_management.file_process import process
 
 
-def exists_word(word, instance):
+def search_in_fila(word, instance, has_conteudo):
     word_lower = word.lower()
     size = instance.__len__()
     qtd_lin = "qtd_linhas"
@@ -16,7 +16,12 @@ def exists_word(word, instance):
         ocorrencias = []
         for j in range(int(item[qtd_lin])):
             if word_lower in item[lin_arq][j].lower():
-                ocorrencias.append({"linha": j + 1})
+                if has_conteudo:
+                    ocorrencias.append(
+                        {"linha": j + 1, "conteudo": item[lin_arq][j]}
+                    )
+                else:
+                    ocorrencias.append({"linha": j + 1})
         if len(ocorrencias):
             returned_list.append(
                 {
@@ -25,9 +30,39 @@ def exists_word(word, instance):
                     "ocorrencias": ocorrencias,
                 }
             )
-        print("RETURNED LIST:", returned_list)
+        # print("RETURNED LIST:", returned_list)
 
     return returned_list
+
+
+def exists_word(word, instance):
+    search_result = search_in_fila(word, instance, False)
+    return search_result
+    # word_lower = word.lower()
+    # size = instance.__len__()
+    # qtd_lin = "qtd_linhas"
+    # lin_arq = "linhas_do_arquivo"
+    # nom_arq = "nome_do_arquivo"
+
+    # returned_list = []
+
+    # for i in range(size):
+    #     item = instance.search(i)
+    #     ocorrencias = []
+    #     for j in range(int(item[qtd_lin])):
+    #         if word_lower in item[lin_arq][j].lower():
+    #             ocorrencias.append({"linha": j + 1})
+    #     if len(ocorrencias):
+    #         returned_list.append(
+    #             {
+    #                 "palavra": word,
+    #                 "arquivo": item[nom_arq],
+    #                 "ocorrencias": ocorrencias,
+    #             }
+    #         )
+    #     print("RETURNED LIST:", returned_list)
+
+    # return returned_list
 
 
 """
@@ -36,33 +71,35 @@ Fazer uma função somente para checar se tem, na linha, a palavra procurada
 
 
 def search_by_word(word, instance):
-    word_lower = word.lower()
-    size = instance.__len__()
-    qtd_lin = "qtd_linhas"
-    lin_arq = "linhas_do_arquivo"
-    nom_arq = "nome_do_arquivo"
+    search_result = search_in_fila(word, instance, True)
+    return search_result
+    # word_lower = word.lower()
+    # size = instance.__len__()
+    # qtd_lin = "qtd_linhas"
+    # lin_arq = "linhas_do_arquivo"
+    # nom_arq = "nome_do_arquivo"
 
-    returned_list = []
+    # returned_list = []
 
-    for i in range(size):
-        item = instance.search(i)
-        ocorrencias = []
-        for j in range(int(item[qtd_lin])):
-            if word_lower in item[lin_arq][j].lower():
-                ocorrencias.append(
-                    {"linha": j + 1, "conteudo": item[lin_arq][j]}
-                )
-        if len(ocorrencias):
-            returned_list.append(
-                {
-                    "palavra": word,
-                    "arquivo": item[nom_arq],
-                    "ocorrencias": ocorrencias,
-                }
-            )
-        print("RETURNED LIST:", returned_list)
+    # for i in range(size):
+    #     item = instance.search(i)
+    #     ocorrencias = []
+    #     for j in range(int(item[qtd_lin])):
+    #         if word_lower in item[lin_arq][j].lower():
+    #             ocorrencias.append(
+    #                 {"linha": j + 1, "conteudo": item[lin_arq][j]}
+    #             )
+    #     if len(ocorrencias):
+    #         returned_list.append(
+    #             {
+    #                 "palavra": word,
+    #                 "arquivo": item[nom_arq],
+    #                 "ocorrencias": ocorrencias,
+    #             }
+    #         )
+    #     # print("RETURNED LIST:", returned_list)
 
-    return returned_list
+    # return returned_list
 
 
 # fila01 = Queue()
