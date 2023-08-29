3c3faa75fe609f71cdaff2c027a982054e084a98
Felipe Mendes <felipemadruga@gmail.com>
2023-08-29 15:34:22 -0300

    Conclui requisito 06 - testes de PriorityQueue

---
+++ tests/priority_queue/mocks2.py
@@ -0,0 +1,25 @@
+file1_lines = [
+    "Acima de tudo, é fundamental ressaltar que a"
+    " adoção de políticas descentralizadoras nos obriga"
+    "à análise do levantamento das variáveis envolvidas."
+]
+
+file2_lines = [
+    "Abaixo dos outros"
+    "Joelma era uma doida desvairada,"
+    "não sabia se sabia entender o que desixava de saber"
+    "tanto quanto antes fora."
+    "Entendes tu?"
+]
+
+file1 = {
+    "nome_do_arquivo": "file1.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": file1_lines
+}
+
+file2 = {
+    "nome_do_arquivo": "file2.txt",
+    "qtd_linhas": 5,
+    "linhas_do_arquivo": file2_lines
+}
--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
+from tests.priority_queue.mocks2 import file1, file2
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_example = PriorityQueue()
+    priority_example.enqueue(file1)
+    priority_example.enqueue(file2)
+
+    assert priority_example.is_priority(file1) is True
+    assert priority_example.is_priority(file2) is False
+
+    assert priority_example.__len__() == 2
+    assert priority_example.search(0) == file1
+    assert priority_example.search(1) == file2
+
+    with pytest.raises(IndexError):
+        priority_example.search(999)
+
+    assert priority_example.dequeue() == file1
+    assert priority_example.dequeue() == file2
+    assert priority_example.dequeue() is None
